d9ddc5db1a6cb1f8c44a2f9a76111decf2465665
槇野 <mkjljono@gmail.com>
2025-12-27 22:57:55 +0900

    スキーマ定義

--- app/schemas.py
+++ app/schemas.py
@@ -3,3 +3,16 @@ from datetime import datetime
 
 class PostBase(BaseModel):
     image_url: str
+    caption: str | None = None
+
+  # リクエストスキーマ(必要あれば)
+class PostCreate(PostBase):
+    pass
+
+
+class Post(PostBase):
+    id: int
+    created_at: datetime
+    
+    class Config:
+        from_attributes = True
